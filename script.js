5d7a341f81fbf8b5577e4c2d506449c47215b379
kostinhaJr <114190775+kostinhaJr@users.noreply.github.com>
2025-10-15 13:25:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
-// === CONFIGURAÇÃO ===
-const scriptURL = 'https://script.google.com/macros/s/AKfycbyG-vTPIWgWIYNEX1IKMsK5ikUxz_4ekibphO8U4_vISgDLn-yjtfAmvTw3oywMr6Me/exec'; // <-- Seu link Apps Script
-
-// === CAPTURA O FORMULÁRIO ===
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyG-vTPIWgWIYNEX1IKMsK5ikUxz_4ekibphO8U4_vISgDLn-yjtfAmvTw3oywMr6Me/exec';
 const form = document.getElementById('formViatura');
 const msg = document.getElementById('mensagem');
 
-// === ENVIO DOS DADOS ===
 form.addEventListener('submit', e => {
-  e.preventDefault(); // impede o recarregamento da página
+  e.preventDefault();
 
   const dados = new FormData(form);
 
-fetch(scriptURL, {
-  method: 'POST',
-  body: dados
-})
-.then(response => response.text())
-.then(data => {
-  msg.textContent = "✅ Registro enviado com sucesso!";
-  form.reset();
-})
-.catch(error => {
-  msg.textContent = "❌ Erro de conexão: " + error.message;
+  fetch(scriptURL, {
+    method: 'POST',
+    body: dados
+  })
+  .then(response => response.text())
+  .then(data => {
+    msg.textContent = "✅ Registro enviado com sucesso!";
+    form.reset();
+  })
+  .catch(error => {
+    msg.textContent = "❌ Erro de conexão: " + error.message;
+  });
 });
-
-
